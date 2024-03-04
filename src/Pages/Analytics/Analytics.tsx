5129f5d2dc10836c7e0039c47481d6b39df2244f
AbdoAyman8642 <156570771+AbdoAyman8642@users.noreply.github.com>
2024-03-04 15:01:02 +0200

    ..

--- src/Pages/Analytics/Analytics.tsx
+++ src/Pages/Analytics/Analytics.tsx
@@ -1,25 +1,37 @@
-import { useLocation } from 'react-router-dom';
-import { useAlert } from '../../contexts/alertContext';
-
-import { Stack } from '@mui/material';
-import useLoading from 'hooks/loading';
-import useApp from 'hooks/useApp';
-import useAuthStore from 'store/auth';
+import { Box, Stack } from '@mui/material';
 
 const Analytics: React.FC = () => {
-	const { alert } = useAlert();
-	const setSession = useAuthStore((state) => state.setSession);
-	const { pathname, search } = useLocation();
-	const { push } = useApp();
-	const { loading, setLoading } = useLoading();
-
 	// if (loading) return <ActionLoader position="fixed" />;
 	return (
-		<>
+		<Box>
 			<Stack component="main" direction={'row'} useFlexGap>
 				Analytics
 			</Stack>
-		</>
+			{/* <PieChart
+				series={[
+					{
+						data: [
+							{ id: 0, value: 10, label: 'series A' },
+							{ id: 1, value: 15, label: 'series B' },
+							{ id: 2, value: 20, label: 'series C' }
+						]
+					}
+				]}
+				width={400}
+				height={200}
+			/>
+
+			<LineChart
+				xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+				series={[
+					{
+						data: [2, 5.5, 2, 8.5, 1.5, 5]
+					}
+				]}
+				width={500}
+				height={300}
+			/> */}
+		</Box>
 	);
 };
 
